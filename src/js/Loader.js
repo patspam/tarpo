8ca548fd64b27b02dc8f5413b0e5aa5b0d0cbe8b
Patrick Donelan <pat@patspam.com>
2009-12-11 13:29:41 -0500

    experimental loader branch

--- src/js/Loader.js
+++ src/js/Loader.js
@@ -8,65 +8,70 @@
  * the minimal set required for each page.
  */
 
-var ENABLE_CONSOLE = 0;
-
-var css = [
-	'extjs/resources/css/ext-all.css',
-	'extjs/air/resources/ext-air.css',
-	'css/tarpo.css',
-];
-
-var js = [
-	'adobe/AIRAliases.js',
-	'adobe/AIRIntrospector.js', // this gets filtered depending on ENABLE_CONSOLE
+function loadTarpo(page) {
+		
+	var ENABLE_CONSOLE = 0;
 	
-	'extjs/adapter/ext/ext-base.js',
-	'extjs/ext-all.js',
-	'extjs/air/ext-air.js',
+	var css = [
+		'extjs/resources/css/ext-all.css',
+		'extjs/air/resources/ext-air.css',
+		'css/tarpo.css',
+	];
 	
-	'underscore/underscore-min.js',
+	var js = [
+		'adobe/AIRAliases.js',
+		'adobe/AIRIntrospector.js', // this gets filtered depending on ENABLE_CONSOLE
+		
+		'extjs/adapter/ext/ext-base.js',
+		'extjs/ext-all.js',
+		'extjs/air/ext-air.js',
+		
+		'underscore/underscore-min.js',
+		
+		'js/Settings.js',
+		'js/Util.js',
+		'js/Overrides.js',
+		
+		'js/Db.js',
+		'js/Upgrade.js',
+		'js/Data.js',
+		'js/Actions.js',
+		'js/Templates.js',
+		'js/DogColours.js',
+		
+		'js/Report.js',
+		'js/Store/Visit.js',
+		'js/Store/Surg.js',
+		'js/Store/Med.js',
+		'js/Store/List.js',
+		'js/EditorGridPanel/Visit.js',
+		'js/EditorGridPanel/Surg.js',
+		'js/EditorGridPanel/Med.js',
+		'js/SelectBox.js',
+		'js/DateTimeField.js',
+		'js/ListLoader.js',
+		'js/TreePanel/List.js',
+		'js/TreeSelector/TreeSelector.js',
+		'js/TreeSelector/List.js',
+		'js/Form.js',
+		'js/WindowManager.js',
+		'js/XmlExporter.js',
+		'js/CsvExporter.js',
+	];
 	
-	'js/Settings.js',
-	'js/Util.js',
-	'js/Overrides.js',
+	page && js.push(page);
 	
-	'js/Db.js',
-	'js/Upgrade.js',
-	'js/Data.js',
-	'js/Actions.js',
-	'js/Templates.js',
-	'js/DogColours.js',
+	document.write(
+		css
+		.map(function(src){ return '<link rel="stylesheet" type="text/css" href="../' + src + '" />' })
+		.join('\n')
+	);
 	
-	'js/Report.js',
-	'js/Store/Visit.js',
-	'js/Store/Surg.js',
-	'js/Store/Med.js',
-	'js/Store/List.js',
-	'js/EditorGridPanel/Visit.js',
-	'js/EditorGridPanel/Surg.js',
-	'js/EditorGridPanel/Med.js',
-	'js/SelectBox.js',
-	'js/DateTimeField.js',
-	'js/ListLoader.js',
-	'js/TreePanel/List.js',
-	'js/TreeSelector/TreeSelector.js',
-	'js/TreeSelector/List.js',
-	'js/Form.js',
-	'js/WindowManager.js',
-	'js/XmlExporter.js',
-	'js/CsvExporter.js',
-];
-
-document.write(
-	css
-	.map(function(src){ return '<link rel="stylesheet" type="text/css" href="../' + src + '" />' })
-	.join('\n')
-);
-
-var debug_re = /AIRIntrospector/;
-document.write(
-	js
-	.filter(function(src){ return ENABLE_CONSOLE || !debug_re.test(src) })
-	.map(function(src){ return '<script src="../' + src + '"></script>' })
-	.join('\n')
-);
+	var debug_re = /AIRIntrospector/;
+	document.write(
+		js
+		.filter(function(src){ return ENABLE_CONSOLE || !debug_re.test(src) })
+		.map(function(src){ return '<script src="../' + src + '"></script>' })
+		.join('\n')
+	);
+}
